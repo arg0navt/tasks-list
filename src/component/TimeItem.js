4591bdb990893ccec36261b50cc8206af05aa4b2
argonavt11 <argonavt.al@yandex.ru>
2018-05-19 17:08:30 +0300

    feat: graggable

--- src/component/TimeItem.js
+++ src/component/TimeItem.js
@@ -2,18 +2,46 @@ import React from "react";
 import Hammer from "react-hammerjs";
 
 export default class TimeItem extends React.Component {
-  componentDidMount() {
-    setTimeout(() => this.widthWrap = this.props.wrap.refs.line.clientWidth)
+  constructor(props){
+    super(props);
+    this.state = {
+      start: null,
+      end: null,
+    };
+  }
+  componentWillMount() {
+    this.setState({
+      start: ((Number(this.props.start.slice(0, 2)) / 24) * 100) + (Number(this.props.start.slice(2, 4) / 1440) * 100),
+      end: ((Number(this.props.end.slice(0, 2)) / 24) * 100) + (Number(this.props.end.slice(2, 4) / 1440) * 100)
+    })
   }
   dragElement(e) {
-    console.log(e, e.distance / (this.widthWrap / 100))
+    e.target.style.marginLeft = `${e.deltaX}px`;
+  }
+  dragEnd(e) {
+    const widthWrap = this.props.wrap.refs.line.clientWidth;
+    if(e.deltaX < 0) {
+      const range = ((e.deltaX * -1) / widthWrap) * 100;
+      this.setState({
+        start: this.state.start - range,
+        end: this.state.end - range
+      }, () => {
+        e.target.style.marginLeft = 0;
+      })
+    } else {
+      const range = (e.deltaX / widthWrap) * 100;
+      this.setState({
+        start: this.state.start + range,
+        end: this.state.end + range
+      }, () => {
+        e.target.style.marginLeft = 0;
+      })
+    }
   }
   render() {
-    this.startPosition = ((Number(this.props.start.slice(0, 2)) / 24) * 100) + (Number(this.props.start.slice(2, 4) / 1440) * 100);
-    this.endPosition = ((Number(this.props.end.slice(0, 2)) / 24) * 100) + (Number(this.props.end.slice(2, 4) / 1440) * 100);
     return (
-      <Hammer onPan={(e) => this.dragElement(e)}>
-        <div className="time-line__item" style={{width: `${this.endPosition - this.startPosition}%`, left: `${this.startPosition}%`}}>
+      <Hammer onPan={(e) => this.dragElement(e)} onPanEnd={(e) => this.dragEnd(e)}>
+        <div className="time-line__item" style={{width: `${this.state.end - this.state.start}%`, left: `${this.state.start}%`}}>
           <div className="time-line__control start"/>
           <Hammer>
             <div className="time-line__control end"/>
