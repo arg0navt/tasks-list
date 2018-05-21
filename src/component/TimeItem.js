2e27602b2c902d8a69559a5e0403f6b4a72328a8
argonavt11 <argonavt.al@yandex.ru>
2018-05-21 23:24:36 +0300

    feat: min and max limit

--- src/component/TimeItem.js
+++ src/component/TimeItem.js
@@ -10,24 +10,53 @@ export default class TimeItem extends React.Component {
     };
   }
   componentWillMount() {
-    console.log(((Number(this.props.start.slice(0, 2)) / 24) * 100) + (Number(this.props.start.slice(2, 4) / 1440) * 100));
     this.setState({
       start: ((Number(this.props.start.slice(0, 2)) / 24) * 100) + (Number(this.props.start.slice(2, 4) / 1440) * 100),
       end: ((Number(this.props.end.slice(0, 2)) / 24) * 100) + (Number(this.props.end.slice(2, 4) / 1440) * 100)
     })
   }
+  dragElement(e) {
+    const widthWrap = this.props.wrap.refs.line.clientWidth;
+    const range = e.deltaX < 0 ? ((e.deltaX * -1) / (widthWrap / 100)) : (e.deltaX / (widthWrap / 100));
+    if(e.deltaX < 0) {
+      if (this.state.start - range >= 0) {
+        e.target.style.marginLeft = `${e.deltaX}px`;
+      } else e.target.style.marginLeft = `-${this.state.start * (widthWrap / 100)}px`;
+    } else {
+      if(this.state.end + range <= 100) {
+        e.target.style.marginLeft = `${e.deltaX}px`;
+      } else {
+        e.target.style.marginLeft = `${(100 - (this.state.end - this.state.start) - this.state.start) * (widthWrap / 100)}px`;
+      }
+    }
+  }
   dragEnd(e) {
     const widthWrap = this.props.wrap.refs.line.clientWidth;
     this.setState(prevState => {
       const range = e.deltaX < 0 ? ((e.deltaX * -1) / (widthWrap / 100)) : (e.deltaX / (widthWrap / 100));
-      prevState.start = e.deltaX < 0 ? prevState.start - range : prevState.start + range;
-      prevState.end = e.deltaX < 0 ? prevState.end - range : prevState.end + range;
+      if(e.deltaX < 0) {
+        if(this.state.start - range <= 0){
+          prevState.end = this.state.end - this.state.start;
+          prevState.start = 0;
+        } else {
+          prevState.start = prevState.start - range;
+          prevState.end = prevState.end - range;
+        }
+      } else {
+        if(this.state.end + range >= 100) {
+          prevState.start = 100 - (this.state.end - this.state.start);
+          prevState.end = 100;
+        } else {
+          prevState.start = prevState.start + range;
+          prevState.end = prevState.end + range;
+        }
+      }
       return prevState;
     }, () => e.target.style.marginLeft = 0);
   }
   render() {
     return (
-      <Hammer onPan={(e) => e.target.style.marginLeft = `${e.deltaX}px`} onPanEnd={(e) => this.dragEnd(e)}>
+      <Hammer onPan={(e) => this.dragElement(e)} onPanEnd={(e) => this.dragEnd(e)}>
         <div className="time-line__item" style={{width: `${this.state.end - this.state.start}%`, left: `${this.state.start}%`}}>
           <div className="time-line__control start"/>
           <Hammer>
