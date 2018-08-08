08bc6278b0ef40b6a21fff852a9cd35c1c3078d6
argonavt11 <argonavt.al@yandex.ru>
2018-08-08 22:32:18 +0300

    feat:  hide state

--- src/component/index.js
+++ src/component/index.js
@@ -8,27 +8,48 @@ type Props = {
 
 const timeMeasure = () => {
   const result = [];
-  for(let i = -1; i <= 47; i++) {
-    if(i < 0) {result.push("00:00")}
-    else if (i === 47) {result.push("")}
-    else {result.push(`${i/2 < 9 ? '0'+ (i/2).toFixed() : (i/2).toFixed()}:${!(i % 2) ? 30 : '00'}`)}
+  for (let i = -1; i <= 47; i++) {
+    if (i < 0) {
+      result.push("00:00")
+    }
+    else if (i === 47) {
+      result.push("")
+    }
+    else {
+      result.push(`${i / 2 < 9 ? '0' + (i / 2).toFixed() : (i / 2).toFixed()}:${!(i % 2) ? 30 : '00'}`)
+    }
   }
   return result;
 };
 
 export default class TasksList extends React.Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    }
+  }
+  onChange = (e) => {
+    const reg = new RegExp(/\d/g);
+    const text = e.target.value.replace(reg, "");
+    this.setState({text});
+  };
   render() {
     return (
-      <div className="taskList">
-        <div className="taskList__users">
-          {this.props.tasks.map((task) => <User key={task.id} name={task.name} />)}
-        </div>
-        <div className="taskList__usersTime">
-          {this.props.tasks.map((task) => <TimeLine key={task.id} timeSlices={task.timeLine} />)}
-          <div className="time-measure">
-            {timeMeasure().map((item) => <div key={item} className="time-measure__item"><p>{item}</p></div>)}
+      <div>
+        <div className="taskList">
+          <div className="taskList__users">
+            {this.props.tasks.map((task) => <User key={task.id} name={task.name}/>)}
+          </div>
+          <div className="taskList__usersTime">
+            {this.props.tasks.map((task) => <TimeLine key={task.id} timeSlices={task.timeLine}/>)}
+            <div className="time-measure">
+              {timeMeasure().map((item) => <div key={item} className="time-measure__item"><p>{item}</p></div>)}
+            </div>
           </div>
         </div>
+        <input type="text" defaultValue={this.state.text} onChange={(e) => this.onChange(e)}/>
+        {/*{this.state.text}*/}
       </div>
     )
   }
